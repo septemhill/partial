7756305e2e56717bb8f2451c2730693fce3340c1
Septem Li <septemhill@gmail.com>
2023-08-14 12:02:25 +0800

    chore: use function wrapper to remove type inference manually

--- partial.go
+++ partial.go
@@ -1,102 +1,126 @@
 package partial
 
-type FnArg0[A any] func() A
+type fnArg0[A any] func() A
 
-type FnArg1[A, B any] func(A) B
+func FnArg0[A any](fn fnArg0[A]) fnArg0[A] {
+	return fn
+}
 
-func (fn FnArg1[A, B]) P1(a A) FnArg0[B] {
+type fnArg1[A, B any] func(A) B
+
+func FnArg1[A, B any](fn fnArg1[A, B]) fnArg1[A, B] {
+	return fn
+}
+
+func (fn fnArg1[A, B]) P1(a A) fnArg0[B] {
 	return func() B {
 		return fn(a)
 	}
 }
 
-type FnArg2[A, B, C any] func(A, B) C
+type fnArg2[A, B, C any] func(A, B) C
 
-func (fn FnArg2[A, B, C]) P1(a A) FnArg1[B, C] {
+func FnArg2[A, B, C any](fn fnArg2[A, B, C]) fnArg2[A, B, C] {
+	return fn
+}
+
+func (fn fnArg2[A, B, C]) P1(a A) fnArg1[B, C] {
 	return func(b B) C {
 		return fn(a, b)
 	}
 }
 
-func (fn FnArg2[A, B, C]) P2(a A, b B) FnArg0[C] {
+func (fn fnArg2[A, B, C]) P2(a A, b B) fnArg0[C] {
 	return func() C {
 		return fn(a, b)
 	}
 }
 
-type FnArg3[A, B, C, D any] func(A, B, C) D
+type fnArg3[A, B, C, D any] func(A, B, C) D
 
-func (fn FnArg3[A, B, C, D]) P1(a A) FnArg2[B, C, D] {
+func FnArg3[A, B, C, D any](fn fnArg3[A, B, C, D]) fnArg3[A, B, C, D] {
+	return fn
+}
+
+func (fn fnArg3[A, B, C, D]) P1(a A) fnArg2[B, C, D] {
 	return func(b B, c C) D {
 		return fn(a, b, c)
 	}
 }
 
-func (fn FnArg3[A, B, C, D]) P2(a A, b B) FnArg1[C, D] {
+func (fn fnArg3[A, B, C, D]) P2(a A, b B) fnArg1[C, D] {
 	return func(c C) D {
 		return fn(a, b, c)
 	}
 }
 
-func (fn FnArg3[A, B, C, D]) P3(a A, b B, c C) FnArg0[D] {
+func (fn fnArg3[A, B, C, D]) P3(a A, b B, c C) fnArg0[D] {
 	return func() D {
 		return fn(a, b, c)
 	}
 }
 
-type FnArg4[A, B, C, D, E any] func(A, B, C, D) E
+type fnArg4[A, B, C, D, E any] func(A, B, C, D) E
 
-func (fn FnArg4[A, B, C, D, E]) P1(a A) FnArg3[B, C, D, E] {
+func FnArg4[A, B, C, D, E any](fn fnArg4[A, B, C, D, E]) fnArg4[A, B, C, D, E] {
+	return fn
+}
+
+func (fn fnArg4[A, B, C, D, E]) P1(a A) fnArg3[B, C, D, E] {
 	return func(b B, c C, d D) E {
 		return fn(a, b, c, d)
 	}
 }
 
-func (fn FnArg4[A, B, C, D, E]) P2(a A, b B) FnArg2[C, D, E] {
+func (fn fnArg4[A, B, C, D, E]) P2(a A, b B) fnArg2[C, D, E] {
 	return func(c C, d D) E {
 		return fn(a, b, c, d)
 	}
 }
 
-func (fn FnArg4[A, B, C, D, E]) P3(a A, b B, c C) FnArg1[D, E] {
+func (fn fnArg4[A, B, C, D, E]) P3(a A, b B, c C) fnArg1[D, E] {
 	return func(d D) E {
 		return fn(a, b, c, d)
 	}
 }
 
-func (fn FnArg4[A, B, C, D, E]) P4(a A, b B, c C, d D) FnArg0[E] {
+func (fn fnArg4[A, B, C, D, E]) P4(a A, b B, c C, d D) fnArg0[E] {
 	return func() E {
 		return fn(a, b, c, d)
 	}
 }
 
-type FnArg5[A, B, C, D, E, F any] func(A, B, C, D, E) F
+type fnArg5[A, B, C, D, E, F any] func(A, B, C, D, E) F
 
-func (fn FnArg5[A, B, C, D, E, F]) P1(a A) FnArg4[B, C, D, E, F] {
+func FnArg5[A, B, C, D, E, F any](fn fnArg5[A, B, C, D, E, F]) fnArg5[A, B, C, D, E, F] {
+	return fn
+}
+
+func (fn fnArg5[A, B, C, D, E, F]) P1(a A) fnArg4[B, C, D, E, F] {
 	return func(b B, c C, d D, e E) F {
 		return fn(a, b, c, d, e)
 	}
 }
 
-func (fn FnArg5[A, B, C, D, E, F]) P2(a A, b B) FnArg3[C, D, E, F] {
+func (fn fnArg5[A, B, C, D, E, F]) P2(a A, b B) fnArg3[C, D, E, F] {
 	return func(c C, d D, e E) F {
 		return fn(a, b, c, d, e)
 	}
 }
 
-func (fn FnArg5[A, B, C, D, E, F]) P3(a A, b B, c C) FnArg2[D, E, F] {
+func (fn fnArg5[A, B, C, D, E, F]) P3(a A, b B, c C) fnArg2[D, E, F] {
 	return func(d D, e E) F {
 		return fn(a, b, c, d, e)
 	}
 }
 
-func (fn FnArg5[A, B, C, D, E, F]) P4(a A, b B, c C, d D) FnArg1[E, F] {
+func (fn fnArg5[A, B, C, D, E, F]) P4(a A, b B, c C, d D) fnArg1[E, F] {
 	return func(e E) F {
 		return fn(a, b, c, d, e)
 	}
 }
 
-func (fn FnArg5[A, B, C, D, E, F]) P5(a A, b B, c C, d D, e E) FnArg0[F] {
+func (fn fnArg5[A, B, C, D, E, F]) P5(a A, b B, c C, d D, e E) fnArg0[F] {
 	return func() F {
 		return fn(a, b, c, d, e)
 	}
--- partial_test.go
+++ partial_test.go
@@ -12,7 +12,7 @@ func TestFnArg1(t *testing.T) {
 	}
 
 	t.Run("FnArg1 P1 Case", func(t *testing.T) {
-		r1 := FnArg1[int, int](sum).P1(1)()
+		r1 := FnArg1(sum).P1(1)()
 		assert.Equal(t, sum(1), r1)
 	})
 }
@@ -28,12 +28,12 @@ func TestFnArg2(t *testing.T) {
 	})
 
 	t.Run("FnArg2 P1 Case", func(t *testing.T) {
-		r1 := FnArg2[int, int, int](sum).P1(1)(2)
+		r1 := FnArg2(sum).P1(1)(2)
 		assert.Equal(t, sum(1, 2), r1)
 	})
 
 	t.Run("FnArg2 P2 Case", func(t *testing.T) {
-		r1 := FnArg2[int, int, int](sum).P2(1, 2)()
+		r1 := FnArg2(sum).P2(1, 2)()
 		assert.Equal(t, sum(1, 2), r1)
 	})
 }
@@ -49,20 +49,20 @@ func TestFnArg3(t *testing.T) {
 	})
 
 	t.Run("FnArg3 P1 Case", func(t *testing.T) {
-		r1 := FnArg3[int, int, int, int](sum).P1(1)(2, 3)
+		r1 := FnArg3(sum).P1(1)(2, 3)
 		assert.Equal(t, sum(1, 2, 3), r1)
 
-		r2 := FnArg3[int, int, int, int](sum).P1(1).P1(2)(3)
+		r2 := FnArg3(sum).P1(1).P1(2)(3)
 		assert.Equal(t, sum(1, 2, 3), r2)
 	})
 
 	t.Run("FnArg3 P2 Case", func(t *testing.T) {
-		r1 := FnArg3[int, int, int, int](sum).P2(1, 2)(3)
+		r1 := FnArg3(sum).P2(1, 2)(3)
 		assert.Equal(t, sum(1, 2, 3), r1)
 	})
 
 	t.Run("FnArg3 P3 Case", func(t *testing.T) {
-		r1 := FnArg3[int, int, int, int](sum).P3(1, 2, 3)()
+		r1 := FnArg3(sum).P3(1, 2, 3)()
 		assert.Equal(t, sum(1, 2, 3), r1)
 	})
 }
@@ -78,34 +78,34 @@ func TestFnArg4(t *testing.T) {
 	})
 
 	t.Run("FnArg4 P1 Case", func(t *testing.T) {
-		r1 := FnArg4[int, int, int, int, int](sum).P1(1)(2, 3, 4)
+		r1 := FnArg4(sum).P1(1)(2, 3, 4)
 		assert.Equal(t, sum(1, 2, 3, 4), r1)
 
-		r2 := FnArg4[int, int, int, int, int](sum).P1(1).P1(2)(3, 4)
+		r2 := FnArg4(sum).P1(1).P1(2)(3, 4)
 		assert.Equal(t, sum(1, 2, 3, 4), r2)
 
-		r3 := FnArg4[int, int, int, int, int](sum).P1(1).P1(2).P1(3)(4)
+		r3 := FnArg4(sum).P1(1).P1(2).P1(3)(4)
 		assert.Equal(t, sum(1, 2, 3, 4), r3)
 
-		r4 := FnArg4[int, int, int, int, int](sum).P1(1).P2(2, 3)(4)
+		r4 := FnArg4(sum).P1(1).P2(2, 3)(4)
 		assert.Equal(t, sum(1, 2, 3, 4), r4)
 	})
 
 	t.Run("FnArg4 P2 Case", func(t *testing.T) {
-		r1 := FnArg4[int, int, int, int, int](sum).P2(1, 2)(3, 4)
+		r1 := FnArg4(sum).P2(1, 2)(3, 4)
 		assert.Equal(t, sum(1, 2, 3, 4), r1)
 
-		r2 := FnArg4[int, int, int, int, int](sum).P2(1, 2).P1(3)(4)
+		r2 := FnArg4(sum).P2(1, 2).P1(3)(4)
 		assert.Equal(t, sum(1, 2, 3, 4), r2)
 	})
 
 	t.Run("FnArg4 P3 Case", func(t *testing.T) {
-		r1 := FnArg4[int, int, int, int, int](sum).P3(1, 2, 3)(4)
+		r1 := FnArg4(sum).P3(1, 2, 3)(4)
 		assert.Equal(t, sum(1, 2, 3, 4), r1)
 	})
 
 	t.Run("FnArg4 P4 Case", func(t *testing.T) {
-		r1 := FnArg4[int, int, int, int, int](sum).P4(1, 2, 3, 4)()
+		r1 := FnArg4(sum).P4(1, 2, 3, 4)()
 		assert.Equal(t, sum(1, 2, 3, 4), r1)
 	})
 }
@@ -119,62 +119,61 @@ func TestFnArg5(t *testing.T) {
 		r1 := FnArg5[int, int, int, int, int, int](sum)(1, 2, 3, 4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r1)
 	})
-
 	t.Run("FnArg5 P1 Case", func(t *testing.T) {
-		r1 := FnArg5[int, int, int, int, int, int](sum).P1(1)(2, 3, 4, 5)
+		r1 := FnArg5(sum).P1(1)(2, 3, 4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r1)
 
-		r2 := FnArg5[int, int, int, int, int, int](sum).P1(1).P1(2)(3, 4, 5)
+		r2 := FnArg5(sum).P1(1).P1(2)(3, 4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r2)
 
-		r3 := FnArg5[int, int, int, int, int, int](sum).P1(1).P1(2).P1(3)(4, 5)
+		r3 := FnArg5(sum).P1(1).P1(2).P1(3)(4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r3)
 
-		r4 := FnArg5[int, int, int, int, int, int](sum).P1(1).P1(2).P1(3).P1(4)(5)
+		r4 := FnArg5(sum).P1(1).P1(2).P1(3).P1(4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r4)
 
-		r5 := FnArg5[int, int, int, int, int, int](sum).P1(1).P2(2, 3)(4, 5)
+		r5 := FnArg5(sum).P1(1).P2(2, 3)(4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r5)
 
-		r6 := FnArg5[int, int, int, int, int, int](sum).P1(1).P2(2, 3).P1(4)(5)
+		r6 := FnArg5(sum).P1(1).P2(2, 3).P1(4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r6)
 
-		r7 := FnArg5[int, int, int, int, int, int](sum).P1(1).P3(2, 3, 4)(5)
+		r7 := FnArg5(sum).P1(1).P3(2, 3, 4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r7)
 
-		r8 := FnArg5[int, int, int, int, int, int](sum).P1(1).P1(2).P2(3, 4)(5)
+		r8 := FnArg5(sum).P1(1).P1(2).P2(3, 4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r8)
 	})
 
 	t.Run("FnArg5 P2 Case", func(t *testing.T) {
-		r1 := FnArg5[int, int, int, int, int, int](sum).P2(1, 2)(3, 4, 5)
+		r1 := FnArg5(sum).P2(1, 2)(3, 4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r1)
 
-		r2 := FnArg5[int, int, int, int, int, int](sum).P2(1, 2).P2(3, 4)(5)
+		r2 := FnArg5(sum).P2(1, 2).P2(3, 4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r2)
 
-		r3 := FnArg5[int, int, int, int, int, int](sum).P2(1, 2).P1(3).P1(4)(5)
+		r3 := FnArg5(sum).P2(1, 2).P1(3).P1(4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r3)
 
-		r4 := FnArg5[int, int, int, int, int, int](sum).P2(1, 2).P1(3)(4, 5)
+		r4 := FnArg5(sum).P2(1, 2).P1(3)(4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r4)
 	})
 
 	t.Run("FnArg5 P3 Case", func(t *testing.T) {
-		r1 := FnArg5[int, int, int, int, int, int](sum).P3(1, 2, 3)(4, 5)
+		r1 := FnArg5(sum).P3(1, 2, 3)(4, 5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r1)
 
-		r2 := FnArg5[int, int, int, int, int, int](sum).P3(1, 2, 3).P1(4)(5)
+		r2 := FnArg5(sum).P3(1, 2, 3).P1(4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r2)
 	})
 
 	t.Run("FnArg5 P4 Case", func(t *testing.T) {
-		r1 := FnArg5[int, int, int, int, int, int](sum).P4(1, 2, 3, 4)(5)
+		r1 := FnArg5(sum).P4(1, 2, 3, 4)(5)
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r1)
 	})
 
 	t.Run("FnArg5 P5 Case", func(t *testing.T) {
-		r1 := FnArg5[int, int, int, int, int, int](sum).P5(1, 2, 3, 4, 5)()
+		r1 := FnArg5(sum).P5(1, 2, 3, 4, 5)()
 		assert.Equal(t, sum(1, 2, 3, 4, 5), r1)
 	})
 }
